d139b351f0580a0f332a98490d7893dbeb9ff385
Vincent <vincent@pinoysme.com>
2021-09-06 09:41:29 +0800

    inital upload of yii files

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=money_market',
+    'username' => 'vincent',
+    'password' => 'adm1ndro1d',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
